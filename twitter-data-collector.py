fb55d0ff4de9c19b50cc85d373949830510de547
Kiarash Kiani <hi@kiarash.info>
2020-10-16 18:15:41 +0330

    update data collector to twitter's streaming API

--- twitter-data-collector.py
+++ twitter-data-collector.py
@@ -1,19 +1,42 @@
-from os import access
-import twitter
+import sys
+import tweepy
 import config
+import pandas as pd
 
-def collect_tweets(term: str, count: int=10) -> list:
-    api = twitter.Api(consumer_key=config.api_key,
-                      consumer_secret=config.api_secret_key, 
-                      access_token_key=config.access_token, 
-                      access_token_secret=config.access_token_secret)
+auth = tweepy.OAuthHandler(config.api_key, config.api_secret_key)
+auth.set_access_token(config.access_token, config.access_token_secret)
+api = tweepy.API(auth)
+df = pd.DataFrame(columns=['tweet_id', 'text', 'likes', 'retweets'])
 
-    return api.GetSearch(term, count=count)
+class CustomStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        df.loc[len(df)] = [
+                status.id,
+                status.text,
+                status.favorite_count,
+                status.retweet_count
+            ]
+        
+        print(f'number of fetched tweets: {len(df)} ', end='\r')
+        sys.stdout.flush()
 
-def collect_tweets_text(term: str, count: int=10) -> list:
-    tweets = collect_tweets(term, count)
-    return [tweet.text for tweet in tweets]
+
+    def on_error(self, status_code):
+        print >> sys.stderr, 'Encountered error with status code:', status_code
+        return True # Don't kill the stream
+
+    def on_timeout(self):
+        print >> sys.stderr, 'Timeout...'
+        return True # Don't kill the stream
+
 
 
 if __name__ == '__main__':
-    print(collect_tweets_text('#trump', 10))
+    print('Press CTR+C to exit ...')
+    try:
+        streamingAPI = tweepy.streaming.Stream(auth, CustomStreamListener())
+        streamingAPI.filter(track=['iphone12', 'iphone 12'], languages=["en"])
+    except KeyboardInterrupt:
+        print(len(df))
+        df.to_csv('tweets.csv', sep=';')
+        sys.exit(0)
